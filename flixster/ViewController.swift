aef2756f669a84ef77bb0a165414ce69d55227fb
Umar Khalid <umark96@hotmail.com>
2020-09-26 16:28:35 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Umar Khalid on 9/26/20.
+//  Copyright © 2020 KhalidApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
